7fc098b780e3acf1bf53e1ade2fb78e4e642ba47
Ale <alejdelcomp@gmail.com>
2019-10-21 22:14:26 -0600

    Update tests_class.py

--- tests_class.py
+++ tests_class.py
@@ -1,9 +1,15 @@
 import unittest
+from django.urls import reverse
 
 class testapp(unittest.TestCase):
     
-    def html(self):
+    def htmltest():
         url = reverse('index')
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'index.html')
+        print("PASSED")
+        
+        
+
+
